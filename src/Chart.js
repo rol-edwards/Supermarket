f5df6915ae4d00a7b0391d11f98b122fb1fa4c35
Roland Edwards <rol.edwards@live.com>
2018-12-18 13:47:10 +0000

    spaced out labels on weekly chart

--- src/Chart.js
+++ src/Chart.js
@@ -3,21 +3,28 @@ import { Chart, ArgumentAxis, ValueAxis, LineSeries } from "@devexpress/dx-react
 import { Scale } from '@devexpress/dx-react-chart';
 import { Legend } from '@devexpress/dx-react-chart-material-ui';
 import moment from 'moment'
+import { scaleTime } from 'd3-scale'
 
 class Chartx extends Component {
   constructor(props){
     super(props);
-    this.getLabel = this.getLabel.bind(this)
+    this.getLabel = this.getLabel.bind(this);
+    this.count = 0;
 
   }
 
   getLabel(props){
     const { text, x, y} = props;
-
+    this.count += 1
+    console.log(this.count)
+    var label = text;
+    if (this.count % 2 == 0){
+      label = ''
+    }
     return (
       <React.Fragment>
         <p>whatever</p>
-        <ArgumentAxis.Label text={text.slice(5, 10)} x={x} y={y} dominantBaseline='hanging'/>
+        <ArgumentAxis.Label text={label} x={x} y={y} dominantBaseline='hanging'/>
       </React.Fragment>);
   }
 
