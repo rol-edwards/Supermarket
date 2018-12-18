f9a4a78b529df5656a2d6fb0799776be1da589a7
Roland Edwards <rol.edwards@live.com>
2018-12-18 14:06:11 +0000

    removed working comments from server

--- server/dbConn.js
+++ server/dbConn.js
@@ -5,47 +5,17 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : config.user,
   password : config.password,
-  database : 'supermarche', //change this back!!!!!!!!!!!!!!!!!!!!!!!!!!
+  database : 'supermarket', 
   insecureAuth: true
 });
 
 
 module.exports = {
 	query: function(query, callback){
-		//connection.connect()
 
 		connection.query(query, callback)
 
-		//connection.end()
 	}
 }
-
-
-
-
-
-/*
-function queryToDB(query, callback){
-	connection.connect(function(err){
-		console.log('You are connected')
-	})
-
-	connection.query(query, callback)
-
-	connection.end()
-}
-
-dbConn = {};
-
-dbConn.query = function(query){
-	return 
-	queryToDB(query, function(err, result){
-		if (err){
-			console.log('err is: ' + err)
-		}
-		console.log('solution is: ' + JSON.stringify(result))
-		}
-	)
-}*/
 
 
